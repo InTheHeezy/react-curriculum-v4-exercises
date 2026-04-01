783ae1e1aae1effa199658645e7c32534c580270
Tonny <tonnyan30@gmail.com>
2026-03-31 19:58:45 -0700

    name,age, hobbies

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,24 @@
 
 export default function StudentWork() {
   //add variables here
+  const name = 'Tonny An';
+  const age = 30;
+  const hobbies = ['Video games', 'Card games', 'Anime', 'Movies'];
+
+  const header = 'Hello my name is ' + name + '.';
+  document.getElementById('header').innerHTML = header;
+  const para =
+    'I was born in Washington State and ' +
+    age +
+    ' years old. I am a logical person and that tends to be the best mindset for programming turns out. I have graduated college with a Bachelors in Computer Science and with the current job market, am having trouble breaking into the field. I am excited to have joined Code the Dream to help me achieve my goals.';
+  document.getElementById('paragraph').innerHTML = para;
+
   return (
     <div>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h id="header"></h>
+      <p id="paragraph"> </p>
+      <p id="hobbies"></p>
     </div>
   );
 }
