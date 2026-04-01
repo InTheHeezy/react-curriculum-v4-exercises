d0b685e0082c46cd9de06ba8b9f3fda1f13d7ea9
Tonny <tonnyan30@gmail.com>
2026-03-31 20:23:03 -0700

    Bug fix

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -8,12 +8,12 @@ export default function StudentWork() {
   const hobbies = ['Video games', 'Card games', 'Anime', 'Movies'];
 
   const header = 'Hello my name is ' + name + '.';
-  document.getElementById('header').innerHTML = header;
+  //document.getElementById('header').innerHTML = header;
   const para =
     'I was born in Washington State and ' +
     age +
     ' years old. I am a logical person and that tends to be the best mindset for programming turns out. I have graduated college with a Bachelors in Computer Science and with the current job market, am having trouble breaking into the field. I am excited to have joined Code the Dream to help me achieve my goals.';
-  document.getElementById('paragraph').innerHTML = para;
+  //document.getElementById('paragraph').innerHTML = para;
 
   return (
     <div>
