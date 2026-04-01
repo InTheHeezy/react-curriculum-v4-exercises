cd5b11a34c7f2a0be00f4cfda9b4c82e973d5b75
Tonny <tonnyan30@gmail.com>
2026-03-31 20:45:00 -0700

    Redid everything

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -5,22 +5,33 @@ export default function StudentWork() {
   //add variables here
   const name = 'Tonny An';
   const age = 30;
-  const hobbies = ['Video games', 'Card games', 'Anime', 'Movies'];
+  const hobbies = [
+    { id: 1, hobby: 'Video games' },
+    { id: 2, hobby: 'Trading Card games' },
+    { id: 3, hobby: 'Anime' },
+    { id: 4, hobby: 'Movies' },
+  ];
 
   const header = 'Hello my name is ' + name + '.';
-  //document.getElementById('header').innerHTML = header;
   const para =
     'I was born in Washington State and ' +
     age +
-    ' years old. I am a logical person and that tends to be the best mindset for programming turns out. I have graduated college with a Bachelors in Computer Science and with the current job market, am having trouble breaking into the field. I am excited to have joined Code the Dream to help me achieve my goals.';
-  //document.getElementById('paragraph').innerHTML = para;
+    ' years old. I am a logical person. ' +
+    'I have graduated college with a Bachelors in Computer Science. ' +
+    'However, with the current job market, I am having trouble breaking into the field.' +
+    'I am excited to have joined Code the Dream to help me achieve my goals.';
 
   return (
     <div>
       {/* add JSX here */}
-      <h id="header"></h>
-      <p id="paragraph"> </p>
-      <p id="hobbies"></p>
+      <h>{header}</h>
+      <p>{para}</p>
+      <p>
+        Here are the hobbies I enjoy
+        {hobbies.map((hobby) => (
+          <li key={hobby.id}>{hobby.hobby}</li>
+        ))}
+      </p>
     </div>
   );
 }
