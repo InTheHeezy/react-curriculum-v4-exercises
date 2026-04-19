499e543b8075187e513d46cabd98be5ce302751c
Tonny <tonnyan30@gmail.com>
2026-04-19 14:43:08 -0700

    Import BugEffectLoop

--- src/exercises/lesson-03/studentWork.jsx
+++ src/exercises/lesson-03/studentWork.jsx
@@ -1,11 +1,15 @@
 //Lesson-03 Component Lifecycle, Hooks, State, and Props
 //Exercise: React Bug Hunt – Fix the broken components in this folder
 //Impport components here
+import BugEffectLoop from './BugEffectLoop';
 
 export default function StudentWork() {
   return (
     <div>
       {/* add components here */}
+      <div>
+        <BugEffectLoop />
+      </div>
       <p>Student output will go here</p>
     </div>
   );
