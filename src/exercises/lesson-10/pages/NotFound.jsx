f864f49e84448f03f465b50922c91f1279c55669
Tonny <tonnyan30@gmail.com>
2026-06-05 19:26:41 -0700

    added uselocation

--- src/exercises/lesson-10/pages/NotFound.jsx
+++ src/exercises/lesson-10/pages/NotFound.jsx
@@ -1,11 +1,15 @@
 import { Link, useLocation } from 'react-router-dom';
 
 export default function NotFound() {
-  const pathname = '';
+  const pathname = useLocation().pathname;
 
   return (
     <section>
       <h2>404: Not Found</h2>
+      <p>
+        We are at: <code>{pathname}</code>
+      </p>
+      <Link to="/">Go Home</Link>
     </section>
   );
 }
